f89bf45debb1687836cb9ee45b254dac40507958
Шарапов Илья <1l.sharapov@mail.ru>
2022-01-30 14:26:54 +0300

    v.2

--- public/homePage.js
+++ public/homePage.js
@@ -11,17 +11,21 @@ newLogoutButton.action = () => {
       location.reload();
   })
 }
-ApiConnector.current((response) => {
+ApiConnector.current(response) => {
   if (response.success)
     ProfileWidget.showProfile(response.data);
-})
+  }else {
+  console.log(response.error);
+  }
 
 newRatesBoard.getCourses = () => {
   ApiConnector.getStocks(response => {
     if (response.success) {
       newRatesBoard.clearTable();
       newRatesBoard.fillTable(response.data);
-    }
+    }else {
+      console.log(response.error);
+      }
   })
 }
 newRatesBoard.getCourses();
@@ -29,26 +33,73 @@ setInterval(newRatesBoard.getCourses, 60000);
 
 newMoneyManager.addMoneyCallback = data => {
   ApiConnector.addMoney(data, response => {
-    if (!response.success)
+    if (!response.success) {
       newMoneyManager.setMessage(response.success, `Средства добавлены не были!`);
     ProfileWidget.showProfile(response.data);
     newMoneyManager.setMessage(response.success, `Сумма добавлена!`);
+    }else {
+      moneyManager.setMessage(response.success, response.error);
+    }
   })
 }
 
 newMoneyManager.conversionMoneyCallback = data => {
   ApiConnector.convertMoney(data,response => {
-    if (!response.success)
+    if (!response.success) {
       newMoneyManager.setMessage(response.success, `Средства не конвертированы!`);
     ProfileWidget.showProfile(response.data);
     newMoneyManager.setMessage(response.success, `Средства конвертированы!`);
+    }else {
+      moneyManager.setMessage(response.success, response.error);
+    }
   })
 }
 newMoneyManager.sendMoneyCallback = data => {
   ApiConnector.transferMoney(data, response => {
-    if (!response.success)
+    if (!response.success) {
       newMoneyManager.setMessage(response.success, `Средства не переведены!`);
     ProfileWidget.showProfile(response.data);
     newMoneyManager.setMessage(response.success, `Средства переведены!`);
+    }else {
+      moneyManager.setMessage(response.success, response.error);
+  }
+  })
+}
+
+(function newDataFavorites() {
+  ApiConnector.getFavorites(response => {
+    if (response.success) {
+      newFavoritesWidget.clearTable();
+      newFavoritesWidget.fillTable(response.data);
+      newMoneyManager.updateUsersList(response.data);
+    }
+  })
+})();
+
+newFavoritesWidget.addUserCallback = data => {
+  ApiConnector.addUserToFavorites(data,response => {
+    if (!response.success) {
+      newFavoritesWidget.setMessage(response.success, `Пользователь не добавлен!`);
+    newFavoritesWidget.clearTable();
+    newFavoritesWidget.fillTable(response.data);
+    newMoneyManager.updateUsersList(response.data);
+    newFavoritesWidget.setMessage(response.success, `Пользователь добавлен!`);
+    }else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
+  })
+}
+
+newFavoritesWidget.removeUserCallback = id => {
+  ApiConnector.removeUserFromFavorites(id, response => {
+    if (!response.success) {
+      newFavoritesWidget.setMessage(response.success, `Пользователь не удален!`);
+    newFavoritesWidget.clearTable();
+    newFavoritesWidget.fillTable(response.data);
+    newMoneyManager.updateUsersList(response.data);
+    newFavoritesWidget.setMessage(response.success, `Пользователь удален!`);
+    }else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
   })
 }
